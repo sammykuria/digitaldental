8b5e7cd20632df3909cb731c18cf3c50b8f72848
sammykuria <kungukuria5724@gmail.com>
2025-08-18 11:58:26 +0300

    deploy 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :accounts,  only: [:index, :create,  :destroy, :update]
   resources :stocks, only: [:index, :create, :destroy, :update]
   resources :patients, only: [:index, :create, :destroy, :update]
@@ -7,6 +8,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+ 
+  # Catch-all route to serve React frontend
+  root "home#index"           # Rails root serves the React app
+  get '*path', to: 'home#index', constraints: ->(req) { !req.xhr? && req.format.html? }
+
+ 
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
