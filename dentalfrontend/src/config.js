e62cc6d758ed3cb5b736584b8c481e4542c929ee
sammykuria <kungukuria5724@gmail.com>
2025-08-18 13:57:21 +0300

    yo

--- dentalfrontend/src/config.js
+++ dentalfrontend/src/config.js
@@ -1,6 +1,9 @@
-const API_BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://digitaldental.onrender.com"
-    : "http://127.0.0.1:4000";
+let API_BASE_URL;
+
+if (process.env.NODE_ENV === "production") {
+  API_BASE_URL = "https://digitaldental.onrender.com"; // your backend on Render
+} else {
+  API_BASE_URL = "http://127.0.0.1:4000"; // local dev backend
+}
 
 export default API_BASE_URL;
